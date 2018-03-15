fixed backpropagation error

Notebook/Character_Predicting_RNN_CV.py
@@ -10,10 +10,9 @@ sys.path.append(dir_path)
 import numpy as np
 from common.SimpleRNN import Single_layer_RNN
 from common.optimizer import Adam
+from nltk.corpus import gutenberg
 
-
-
-x = 'hello world! nice to meet you! i love iron-man'
+x = gutenberg.raw('shakespeare-hamlet.txt')[:775]
 
 class chr_coding(object):
     def __init__(self):
@@ -73,7 +72,7 @@ def train(rnn, optim, option=True):
         total_loss_list.append(total_loss)
         total_acc_list.append(acc)
 
-    return total_acc_list[-1]
+    return total_acc_list[-1], test_string
 
 # encoding
 encoder = chr_coding()
@@ -81,13 +80,12 @@ encoder.fit(x)
 one_hot_data = encoder.encode(x)
 
 # global parameters
-NUM_CHECK = 5
-NUM_EPOCHS = 3000
+NUM_CHECK = 3
+NUM_EPOCHS = 500
 INPUT_SIZE = one_hot_data.shape[1]
-HIDDEN_SIZE = 30
+HIDDEN_SIZE = 40
 OUTPUT_SIZE = one_hot_data.shape[1]
-SEQ_LEN = one_hot_data.shape[0] - 1
-BPTT_TRUNCATE = 5
+BPTT_TRUNCATE = 10
 
 # data preparing
 train_x = one_hot_data[:-1]
@@ -109,43 +107,51 @@ def get_models():
                             bptt_truncate=BPTT_TRUNCATE, activation_func='relu')
     optim3 = Adam()
 
-    labels = ['model1: tanh + backward', 'model2: tanh + backward_truncate', 'model3: relu + backward_truncate']
+    labels = ['model1: tanh + backward', 'model2: tanh + backward_truncate', 'model3: relu + backward']
     rnns = [rnn1, rnn2, rnn3]
     optims = [optim1, optim2, optim3]
     return labels, rnns, optims
 
+
 def main():
     print("Number of Checking:", NUM_CHECK)
-    print("="*30)
+    print("="*50)
     total_acc_array = []
+    total_text_array = []
     for i in range(NUM_CHECK):
 
         labels, rnns, optims = get_models()
         acc_list = []
+        text_list = []
         for j, (rnn, optim) in enumerate(zip(rnns, optims)):
-            print("running #", str(i), "| " + labels[j])
-            if j == 0:
-                trun_key = True
+            if j != 1:
+                not_trun_key = True
             else:
-                trun_key = False
+                not_trun_key = False
 
-            accuracy = train(rnn, optim, trun_key)
+            start_time = time.time()
+            accuracy, test_string = train(rnn, optim, not_trun_key)
+            print("runned #", str(i), "| " + labels[j], "| {} second".format(time.time() - start_time))
             acc_list.append(accuracy)
+            text_list.append(test_string)
 
         total_acc_array.append(acc_list)
+        total_text_array.append(text_list)
 
     total_acc_array = np.array(total_acc_array)
+    total_text_array = np.array(total_text_array)
+    max_idxs = np.argmax(total_acc_array, axis=0)
 
     for i in range(len(labels)):
         print("## " + labels[i] + " ##")
         print("Average Score:", np.mean(total_acc_array, axis=0)[i])
         print("Acc_list:", total_acc_array[:, i])
-
+        print("=" * 50)
+        print(total_text_array[i][max_idxs[i]])
+        print("=" * 50)
 
 if __name__ == "__main__":
-    start_time = time.time()
     main()
-    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 

common/SimpleRNN.py
@@ -77,8 +77,7 @@ class Single_layer_RNN(object):
         os = {}
 
         for i in np.arange(self.T):
-            a = self.layers['Affine_hh'][i].forward(hs[i - 1]) + \
-                self.layers['Affine_xh'][i].forward(x[i:i + 1, :])
+            a = self.layers['Affine_hh'][i].forward(hs[i - 1]) + self.layers['Affine_xh'][i].forward(x[i:i + 1, :])
             hs[i] = self.layers['Activation'][i].forward(a)
             os[i] = self.layers['Affine_hy'][i].forward(hs[i])
 
@@ -112,10 +111,15 @@ class Single_layer_RNN(object):
         for t in np.arange(self.T)[::-1]:
             dout = self.last_layers[t].backward()
             dht_raw = self.layers['Affine_hy'][t].backward(dout)
-            dat = self.layers['Activation'][t].backward(dht_raw) + dht
+            dat = self.layers['Activation'][t].backward(dht_raw + dht)
             dht = self.layers['Affine_hh'][t].backward(dat)
             dx = self.layers['Affine_xh'][t].backward(dat)  # dx
-            self._params_summation(t)
+
+            self.params_summ['W_xh'] += self.layers['Affine_xh'][t].dW
+            self.params_summ['W_hh'] += self.layers['Affine_hh'][t].dW
+            self.params_summ['W_hy'] += self.layers['Affine_hy'][t].dW
+            self.params_summ['b_h'] += self.layers['Affine_hh'][t].db
+            self.params_summ['b_y'] += self.layers['Affine_hy'][t].db
 
     def _params_summation_init(self):
         self.params_summ = {}
@@ -125,12 +129,6 @@ class Single_layer_RNN(object):
         self.params_summ['b_h'] = np.zeros_like(self.params['b_h'])
         self.params_summ['b_y'] = np.zeros_like(self.params['b_y'])
 
-    def _params_summation(self, step=None):
-        self.params_summ['W_xh'] += self.layers['Affine_xh'][step].dW
-        self.params_summ['W_hh'] += self.layers['Affine_hh'][step].dW
-        self.params_summ['W_hy'] += self.layers['Affine_hy'][step].dW
-        self.params_summ['b_h'] += self.layers['Affine_hh'][step].db
-        self.params_summ['b_y'] += self.layers['Affine_hy'][step].db
 
     def backward_truncate(self):
         # TBPTT
@@ -144,7 +142,7 @@ class Single_layer_RNN(object):
             self.params_summ['b_y'] += self.layers['Affine_hy'][t].db
 
             for bptt_step in np.arange(max(0, t - self.bptt_truncate), t + 1)[::-1]:
-                dat = self.layers['Activation'][bptt_step].backward(dht_raw) + dht
+                dat = self.layers['Activation'][bptt_step].backward(dht_raw + dht)
                 dht = self.layers['Affine_hh'][bptt_step].backward(dat)
                 dx = self.layers['Affine_xh'][bptt_step].backward(dat)  # dx
                 self.params_summ['W_xh'] += self.layers['Affine_xh'][bptt_step].dW