0f12375da6ca89c8bcc58256dc2f6882c510ad0b
Soo <simonjisu@gmail.com>
2018-03-19 17:05:24 +0900

    fixed legend

--- common/train_graph.py
+++ common/train_graph.py
@@ -7,19 +7,29 @@ def loss_graph(train_loss_list, train_acc_list=None, test_loss_list=None, test_a
     fig, ax1 = plt.subplots(figsize=(8, 8), dpi=90)
     losses = [('b-', 'train_loss'), ('g-', 'test_loss')]
     accs = [('r-', 'train_acc'), ('y-', 'test_acc')]
-    ax1.plot(train_loss_list, losses[0][0], label=losses[0][1])
+    legend_list = []
+    train_l = ax1.plot(train_loss_list, losses[0][0], label=losses[0][1])
+    legend_list += train_l
     if test_loss_list:
-        ax1.plot(test_loss_list, losses[1][0], label=losses[1][1])
+        test_l = ax1.plot(test_loss_list, losses[1][0], label=losses[1][1])
+        legend_list += test_l
     ax1.set_ylabel('Loss')
 
     if train_acc_list:
         ax2 = ax1.twinx()
-        ax2.plot(train_acc_list, accs[0][0], label=accs[0][1])
+        train_a = ax2.plot(train_acc_list, accs[0][0], label=accs[0][1])
+        legend_list += train_a
         if test_acc_list:
-            ax2.plot(train_acc_list, accs[1][0], label=accs[1][1])
+            test_a = ax2.plot(train_acc_list, accs[1][0], label=accs[1][1])
+            legend_list += test_a
         ax2.set_ylabel('Accuracy')
+
+    # set legend
+    labels = [l.get_label() for l in legend_list]
+    ax1.legend(legend_list, labels, loc=0)
+
 
     plt.xlabel('Epoch')
     plt.title('LOSS & ACCURACY GRAPH' if train_acc_list else 'LOSS GRAPH', fontsize=15, y=1.01)
-    plt.legend()
+    # plt.legend()
     plt.show()
